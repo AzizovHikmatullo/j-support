74a715c6c2eca22995491b76719c96540424c26e
AzizovHikmatullo <azizov.hikmatullo07@gmail.com>
2026-03-11 17:58:32 +0500

    add channel init

---
+++ internal/channel/handler.go
@@ -0,0 +1,53 @@
+package channel
+
+import (
+	"net/http"
+
+	"github.com/AzizovHikmatullo/j-support/internal/contacts"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+type handler struct {
+	contactService contacts.Service
+}
+
+func NewHandler(service contacts.Service) *handler {
+	return &handler{
+		contactService: service,
+	}
+}
+
+func (h *handler) InitWeb(c *gin.Context) {
+	var req InitWebRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "invalid body"})
+		return
+	}
+
+	sessionID := "js_" + uuid.Must(uuid.NewV7()).String()
+
+	if err := h.contactService.InitContact(c.Request.Context(), sessionID, req.Name, req.Phone); err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "failed to create contact"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"session_id": sessionID,
+	})
+}
+
+func (h *handler) InitTelegram(c *gin.Context) {
+	var req InitTelegramRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "invalid body"})
+		return
+	}
+
+	if err := h.contactService.InitContact(c.Request.Context(), req.TelegramID, req.Name, req.Phone); err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "failed to create contact"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"ok": true})
+}
--- internal/channel/models.go
+++ internal/channel/models.go
@@ -14,3 +14,14 @@ type Identity struct {
 	ChannelType string
 	ID          string
 }
+
+type InitWebRequest struct {
+	Name  string `json:"name"  binding:"required"`
+	Phone string `json:"phone" binding:"required"`
+}
+
+type InitTelegramRequest struct {
+	TelegramID string `json:"telegram_id"  binding:"required"`
+	Name       string `json:"name"  binding:"required"`
+	Phone      string `json:"phone" binding:"required"`
+}
